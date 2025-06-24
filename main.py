0550c5b4bdbe6daea1bf019196c312fa54b92c5d
chrissch24 <christophscheele.halter@gmail.com>
2025-06-24 09:23:59 +0200

    Update

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ import time
 firmware_url = "https://raw.githubusercontent.com/chrissch24/ota-test/main"
 
 led1 = Pin(4, Pin.OUT)
-#led2 = Pin(5, Pin.OUT)
+led2 = Pin(5, Pin.OUT)
 
 # LED Tester
 led1.value(1)
-#led2.value(1)
+led2.value(1)
 time.sleep(0.5)
 led1.value(0)
-#led2.value(0)
+led2.value(0)
 
 # Hauptschleife
 while True:
     led1.value(1)
-    #led2.value(1)
+    led2.value(1)
     time.sleep(1)
     led1.value(0)
-    #led2.value(0)
+    led2.value(0)
     time.sleep(2)

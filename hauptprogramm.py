111a8527e0dbe57307bf66d1a81b60a58b8d8315
chrissch24 <christophscheele.halter@gmail.com>
2025-06-24 10:22:01 +0200

    Update

--- hauptprogramm.py
+++ hauptprogramm.py
@@ -6,27 +6,27 @@ from WIFI_CONFIG import SSID, PASSWORD
 from machine import Pin
 import time
 
-firmware_url = "https://raw.githubusercontent.com/chrissch24/ota-test/main"
+firmware_url = "https://raw.githubusercontent.com/chrissch24/ota-test/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "hauptprogramm.py")
 
 ota_updater.download_and_install_update_if_available()
 
 led1 = Pin(4, Pin.OUT)
-#led2 = Pin(5, Pin.OUT)
+led2 = Pin(5, Pin.OUT)
 
 # LED Tester
 led1.value(1)
-#led2.value(1)
+led2.value(1)
 time.sleep(0.5)
 led1.value(0)
-#led2.value(0)
+led2.value(0)
 
 # Hauptschleife
 while True:
     led1.value(1)
-    #led2.value(1)
+    led2.value(1)
     time.sleep(1)
     led1.value(0)
-    #led2.value(0)
+    led2.value(0)
     time.sleep(2)

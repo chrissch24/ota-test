ba31a4fd59fd371ec1c19a92798e3d2e5fcf8f27
chrissch24 <christophscheele.halter@gmail.com>
2025-06-24 10:25:37 +0200

    Update

--- hauptprogramm.py
+++ hauptprogramm.py
@@ -12,21 +12,21 @@ ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "hauptprogramm.py")
 
 
 led1 = Pin(4, Pin.OUT)
-#led2 = Pin(5, Pin.OUT)
+led2 = Pin(5, Pin.OUT)
 
 # LED Tester
 led1.value(1)
-#led2.value(1)
+led2.value(1)
 time.sleep(0.5)
 led1.value(0)
-#led2.value(0)
+led2.value(0)
 
 # Hauptschleife
 while True:
     ota_updater.download_and_install_update_if_available()
     led1.value(1)
-    #led2.value(1)
+    led2.value(1)
     time.sleep(1)
     led1.value(0)
-    #led2.value(0)
+    led2.value(0)
     time.sleep(2)
